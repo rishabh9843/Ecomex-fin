7d9cb3f095c960018675ccc61ce03ff0db9c53a8
rishabh9843 <166657089+rishabh9843@users.noreply.github.com>
2025-10-11 21:14:29 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
@@ -10,8 +9,8 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      "/api/": "http://localhost:5000",
-      "/uploads/": "http://localhost:5000",
+      "/api/": "https://ecomex-si8k.onrender.com",
+      "/uploads/": "https://ecomex-si8k.onrender.com",
     },
   },
 });
