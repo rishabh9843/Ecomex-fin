3c7a3a103d922472219b9beeec60aff04e1afc84
rishabh9843 <166657089+rishabh9843@users.noreply.github.com>
2025-10-13 19:35:33 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  root: 'src', // points Vite to src folder
-  build: {
-    outDir: path.resolve(__dirname, '../dist'), // output to frontend/dist
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api/": "https://ecomex-fin.onrender.com/",
+      "/uploads/": "https://ecomex-fin.onrender.com/",
+    },
   },
-})
+});
