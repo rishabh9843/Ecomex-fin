3ebdd0ebdc06c62f6898097e9029482f2193b5b3
rishabh9843 <166657089+rishabh9843@users.noreply.github.com>
2025-10-13 20:34:10 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,16 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api/": {
-        target: process.env.VITE_API_URL || "http://localhost:5000",
-        changeOrigin: true,
-      },
-      "/uploads/": {
-        target: process.env.VITE_UPLOADS_URL || "http://localhost:5000",
-        changeOrigin: true,
-      },
-    },
-  },
 });
