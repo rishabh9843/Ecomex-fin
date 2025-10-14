746cfb983b8542929a2cc6676c6be4736e257ce4
rishabh9843 <166657089+rishabh9843@users.noreply.github.com>
2025-10-14 23:46:18 +0530

    Update apiSlice.js

--- frontend/src/redux/api/apiSlice.js
+++ frontend/src/redux/api/apiSlice.js
@@ -1,7 +1,10 @@
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 import { BASE_URL } from "../constants";
 
-const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
+const baseQuery = fetchBaseQuery({ 
+  baseUrl: BASE_URL,
+  credentials: "include" // Add this line - sends cookies with every request
+});
 
 export const apiSlice = createApi({
   baseQuery,
